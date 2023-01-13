95354f5af0b5c7a2aaee756075e1857589b3670d
royw7 <99209164+royw7@users.noreply.github.com>
2023-01-13 14:03:40 -0500

    Update ex.js

--- ex.js
+++ ex.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
+
 app.get('/', (req,res)=>{
     res.send('Hello there');
 });
@@ -13,7 +15,27 @@ const courses = [
 app.get('/api/courses', (req, res)=>{
     res.send(courses);
 })
+//http post request
 
+app.post('api/courses', (req,res) => {
+    // you write the if code here
+    //add an if statement so that the name of the course you post is .min(3) characters 
+    if(req.name.length >= 3){
+        const course ={
+            //we assign an ID and a name property
+            id: course.length +1,
+            name:req.body.name
+    }
+    courses.push(course)
+    res.send(course)
+}
+            //YOU WRITE THE NEXT LINES OF code
+          //next step: push it to the array
+            //next step: the server should return the new resource to the client in the body of the response
+    
+      
+    });
+    
 app.get('/api/courses/:id', (req,res)=>{
     const course = courses.find(c=>c.id === parseInt(req.params.id));
     if(!course){
